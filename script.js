ed357681810d1c2f2f42a7a052add263416eb8e2
Ervez <fb.blaszkiewicz@gmail.com>
2023-02-05 23:42:01 +0100

    animation add to basket

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     let basket = [];
     let addToBasketButtons;
     const productsSection = document.querySelector(".products");
-    
+    let temp = true;
     const addToBasket = (e) => {
       const selectedId = parseInt(e.target.dataset.id);
     
@@ -34,13 +34,14 @@ $(document).ready(function() {
         <div class="product_price">
         <span class="price">${items[i].price.toFixed(2)} zł</span>
         </div>
-        <button data-id="${
+        <div class="buttons">         <button data-id="${
           items[i].id
-        }" class="product_add_basket_btn">Add to basket</button>`;
+        }" class="cart-button"><span class="add-to-cart">Add to basket</span> <span class="added">Item added</span> <i class="fa fa-shopping-cart"></i> <i class="fa fa-square"></i> </button> </div>
+`;
     
         productsSection.appendChild(newProduct);
       }
-      addToBasketButtons = document.querySelectorAll(".product_add_basket_btn");
+      addToBasketButtons = document.querySelectorAll(".cart-button");
       addToBasketButtons.forEach((btn) =>
         btn.addEventListener("click", addToBasket)
       );
@@ -61,5 +62,24 @@ $(document).ready(function() {
     })
     renderProducts(currentProducts);
 
+          
+    const cartButtons = document.querySelectorAll('.cart-button');
+
+    cartButtons.forEach(button => {
     
+    button.addEventListener('click',cartClick);
+
+    });
+
+    function cartClick(){
+      if(temp == true){
+     let button = this;
+     button.classList.add('clicked');
+     setTimeout( function(){ 
+      $(button).removeClass("clicked");
+      temp = true;
+    } , 2500 );
+    }
+    temp = false;
+  }
 });
